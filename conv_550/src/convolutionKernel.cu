a86711c03dc18288b882de238a45525c63402f6b
Lesia Semenova <lesiakarabin@gmail.com>
2017-12-05 11:11:27 -0500

    Add unroll

--- conv_550/src/convolutionKernel.cu
+++ conv_550/src/convolutionKernel.cu
@@ -1,5 +1,7 @@
 #include "convolution.h"
 
+#define KERNAL_RAD 8
+
 __global__ void convKernelFullNaiveSepKernel(float* d_Input, float* d_Output, float* d_Kernel, int  imageW, int imageH, int kernelR)
 {
     int row = blockDim.y * blockIdx.y + threadIdx.y;
@@ -92,10 +94,10 @@ __global__ void convKernelSeparableRowShared(float* d_Input, float* d_Output, fl
     __shared__ float data[ CACHE_H * (CACHE_W + KERNAL_RAD * 2)];
 
     // Initializations
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
-    int loc = row * imageW + col;
-    int shift = threadIdx.y * (CACHE_W + kernelR * 2);
+    const int row = blockDim.y * blockIdx.y + threadIdx.y;
+    const int col = blockDim.x * blockIdx.x + threadIdx.x;
+    const int loc = row * imageW + col;
+    const int shift = threadIdx.y * (CACHE_W + kernelR * 2);
 
     // left apron
     data[threadIdx.x + shift] = (col - kernelR >= 0) ? d_Input[ loc - kernelR] : 0;
@@ -107,10 +109,59 @@ __global__ void convKernelSeparableRowShared(float* d_Input, float* d_Output, fl
 
     // convolution
     float s = 0;
-    for (int j = -kernelR; j <= kernelR; j++)
-        s += data[kernelR + threadIdx.x + j + shift] * d_Kernel[kernelR - j];
+	for (int j = -kernelR; j <= kernelR; j++)
+	{
+		s += data[kernelR + threadIdx.x + j + shift] * d_Kernel[kernelR - j];
+	}
 
     d_Output[loc] = s;
+}
+
+__global__ void convKernelSeparableRowSharedUnroll(float* d_Input, float* d_Output, float* d_Kernel, int  imageW, int imageH)
+{
+	// Data cache
+	__shared__ float data[CACHE_H * (CACHE_W + KERNAL_RAD * 2)];
+
+	// Initializations
+	const int row = blockDim.y * blockIdx.y + threadIdx.y;
+	const int col = blockDim.x * blockIdx.x + threadIdx.x;
+	const int loc = row * imageW + col;
+	const int shift = threadIdx.y * (CACHE_W + KERNAL_RAD * 2);
+
+	// left apron
+	data[threadIdx.x + shift] = (col - KERNAL_RAD >= 0) ? d_Input[loc - KERNAL_RAD] : 0;
+	// right apron
+	data[threadIdx.x + blockDim.x + shift] = (col + KERNAL_RAD < imageW) ? d_Input[loc + KERNAL_RAD] : 0;
+
+	//compute and store results
+	__syncthreads();
+
+	// convolution
+	float s = 0;
+	/*s = data[KERNAL_RAD + threadIdx.x - 8 + shift] * d_Kernel[16] +
+		data[KERNAL_RAD + threadIdx.x - 7 + shift] * d_Kernel[15] +
+		data[KERNAL_RAD + threadIdx.x - 6 + shift] * d_Kernel[14] +
+		data[KERNAL_RAD + threadIdx.x - 5 + shift] * d_Kernel[13] +
+		data[KERNAL_RAD + threadIdx.x - 4 + shift] * d_Kernel[12] +
+		data[KERNAL_RAD + threadIdx.x - 3 + shift] * d_Kernel[11] +
+		data[KERNAL_RAD + threadIdx.x - 2 + shift] * d_Kernel[10] +
+		data[KERNAL_RAD + threadIdx.x - 1 + shift] * d_Kernel[9] +
+		data[KERNAL_RAD + threadIdx.x + 0 + shift] * d_Kernel[8] +
+		data[KERNAL_RAD + threadIdx.x + 1 + shift] * d_Kernel[7] +
+		data[KERNAL_RAD + threadIdx.x + 2 + shift] * d_Kernel[6] +
+		data[KERNAL_RAD + threadIdx.x + 3 + shift] * d_Kernel[5] +
+		data[KERNAL_RAD + threadIdx.x + 4 + shift] * d_Kernel[4] +
+		data[KERNAL_RAD + threadIdx.x + 5 + shift] * d_Kernel[3] +
+		data[KERNAL_RAD + threadIdx.x + 6 + shift] * d_Kernel[2] +
+		data[KERNAL_RAD + threadIdx.x + 7 + shift] * d_Kernel[1] +
+		data[KERNAL_RAD + threadIdx.x + 8 + shift] * d_Kernel[0];*/
+
+	for (int j = -KERNAL_RAD; j <= KERNAL_RAD; j++)
+	{
+		s += data[KERNAL_RAD + threadIdx.x + j + shift] * d_Kernel[KERNAL_RAD - j];
+	}
+
+	d_Output[loc] = s;
 }
 
 __global__ void convKernelSeparableColumnShared(float* d_Input, float* d_Output, float* d_Kernel, int  imageW, int imageH, int kernelR)
@@ -119,10 +170,10 @@ __global__ void convKernelSeparableColumnShared(float* d_Input, float* d_Output,
     __shared__ float data[CACHE_W * (CACHE_H + KERNAL_RAD * 2)];
 
     // Initializations
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
-    int loc = row * imageW + col;
-    int shift = threadIdx.y * CACHE_W;
+    const int row = blockDim.y * blockIdx.y + threadIdx.y;
+    const int col = blockDim.x * blockIdx.x + threadIdx.x;
+    const int loc = row * imageW + col;
+    const int shift = threadIdx.y * CACHE_W;
 
     // top apron
     data[threadIdx.x + shift] = (row - kernelR >=0) ? d_Input[ loc - imageW * kernelR] : 0;
@@ -141,6 +192,51 @@ __global__ void convKernelSeparableColumnShared(float* d_Input, float* d_Output,
     d_Output[loc] = s;
 }
 
+__global__ void convKernelSeparableColumnSharedUnroll(float* d_Input, float* d_Output, float* d_Kernel, int  imageW, int imageH)
+{
+	// Data cache
+	__shared__ float data[CACHE_W * (CACHE_H + KERNAL_RAD * 2)];
+
+	// Initializations
+	const int row = blockDim.y * blockIdx.y + threadIdx.y;
+	const int col = blockDim.x * blockIdx.x + threadIdx.x;
+	const int loc = row * imageW + col;
+	const int shift = threadIdx.y * CACHE_W;
+
+	// top apron
+	data[threadIdx.x + shift] = (row - KERNAL_RAD >= 0) ? d_Input[loc - imageW * KERNAL_RAD] : 0;
+	// bottom apron
+	data[threadIdx.x + shift + blockDim.y * CACHE_W] = (row + KERNAL_RAD < imageH) ? d_Input[loc + imageW * KERNAL_RAD] : 0;
+
+	//Compute and store results
+	__syncthreads();
+
+	// convolution
+	float s = 0;
+	/* s = data[threadIdx.x + (threadIdx.y + 0) * CACHE_W] * d_Kernel[16] +
+		data[threadIdx.x + (threadIdx.y + 1) * CACHE_W] * d_Kernel[15] +
+		data[threadIdx.x + (threadIdx.y + 2) * CACHE_W] * d_Kernel[14] +
+		data[threadIdx.x + (threadIdx.y + 3) * CACHE_W] * d_Kernel[13] +
+		data[threadIdx.x + (threadIdx.y + 4) * CACHE_W] * d_Kernel[12] +
+		data[threadIdx.x + (threadIdx.y + 5) * CACHE_W] * d_Kernel[11] +
+		data[threadIdx.x + (threadIdx.y + 6) * CACHE_W] * d_Kernel[10] +
+		data[threadIdx.x + (threadIdx.y + 7) * CACHE_W] * d_Kernel[9] +
+		data[threadIdx.x + (threadIdx.y + 8) * CACHE_W] * d_Kernel[8] +
+		data[threadIdx.x + (threadIdx.y + 9) * CACHE_W] * d_Kernel[7] +
+		data[threadIdx.x + (threadIdx.y + 10) * CACHE_W] * d_Kernel[6] +
+		data[threadIdx.x + (threadIdx.y + 11) * CACHE_W] * d_Kernel[5] +
+		data[threadIdx.x + (threadIdx.y + 12) * CACHE_W] * d_Kernel[4] +
+		data[threadIdx.x + (threadIdx.y + 13) * CACHE_W] * d_Kernel[3] +
+		data[threadIdx.x + (threadIdx.y + 14) * CACHE_W] * d_Kernel[2] +
+		data[threadIdx.x + (threadIdx.y + 15) * CACHE_W] * d_Kernel[1] +
+		data[threadIdx.x + (threadIdx.y + 16) * CACHE_W] * d_Kernel[0];*/
+
+	for (int i = -KERNAL_RAD; i <= KERNAL_RAD; i++) {
+		s += data[threadIdx.x + (threadIdx.y + i + KERNAL_RAD) * CACHE_W] * d_Kernel[KERNAL_RAD - i];
+	}
+
+	d_Output[loc] = s;
+}
 
 void convolutionSeparableColumnNaive(dim3 gridSize, dim3 blockSize, float* d_Input,float* d_Output,float* d_Kernel,int  imageW,int imageH,int kernelR)
 {
@@ -162,13 +258,23 @@ void convolutionFullNaive(dim3 gridSize, dim3 blockSize, float* d_Input,float* d
     convKernelFullNaive<< <gridSize, blockSize>> >(d_Input,d_Output,d_Kernel, imageW, imageH, kernelR);
 }
 
-void convolutionSeparableColumnShared(dim3 gridSize, dim3 blockSize, float* d_Input,float* d_Output,float* d_Kernel,int  imageW,int imageH,int kernelR)
+void convolutionSeparableColumnShared(dim3 gridSize, dim3 blockSize, float* d_Input,float* d_Output,float* d_Kernel,int  imageW, int imageH, int kernelR)
 {
     convKernelSeparableColumnShared<< <gridSize, blockSize>> >(d_Input,d_Output,d_Kernel, imageW, imageH, kernelR);
 }
 
-void convolutionSeparableRowShared(dim3 gridSize, dim3 blockSize, float* d_Input,float* d_Output,float* d_Kernel,int  imageW,int imageH,int kernelR)
+void convolutionSeparableColumnSharedUnroll(dim3 gridSize, dim3 blockSize, float* d_Input, float* d_Output, float* d_Kernel, int imageW, int imageH)
+{
+	convKernelSeparableColumnSharedUnroll << <gridSize, blockSize >> >(d_Input, d_Output, d_Kernel, imageW, imageH);
+}
+
+void convolutionSeparableRowShared(dim3 gridSize, dim3 blockSize, float* d_Input,float* d_Output,float* d_Kernel,int imageW, int imageH, int kernelR)
 {
     convKernelSeparableRowShared<< <gridSize, blockSize>> >(d_Input,d_Output,d_Kernel, imageW, imageH, kernelR);
+}
+
+void convolutionSeparableRowSharedUnroll(dim3 gridSize, dim3 blockSize, float* d_Input, float* d_Output, float* d_Kernel, int imageW, int imageH)
+{
+	convKernelSeparableRowSharedUnroll << <gridSize, blockSize >> >(d_Input, d_Output, d_Kernel, imageW, imageH);
 }
 
--- conv_550/src/main.cpp
+++ conv_550/src/main.cpp
@@ -23,6 +23,19 @@ using namespace std;
 int isDividedUp(int a, int b){ return ((a % b) != 0) ? (a / b + 1) : (a / b); }
 bool imgL2error(float* img1, float* img2, const int & imageW, const int & imageH);
 
+void seperableSharedKernelUnroll(
+	float *d_Input,
+	float *d_Output,
+	float *d_Buffer,
+	float *d_Kernel,
+	float *h_Kernel,
+	float *h_Input,
+	float *h_OutputGPUSepShared,
+	const int imageW,
+	const int imageH,
+	const int iterations,
+	StopWatchInterface *hTimer);
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main program
@@ -50,7 +63,7 @@ int main(int argc, char **argv)
 
     const int imageW =  3072;
     const int imageH =  3072;
-    const int iterations = 10;
+    const int iterations = 30;
 
     StopWatchInterface *hTimer = NULL;
 
@@ -88,7 +101,7 @@ int main(int argc, char **argv)
         h_Input[i] = (float)(rand() % 16);
     }
 
-
+	/*
     // Separable convolution on CPU
     {
         printf("Running CPU separable convolution (%u identical iterations).\n", iterations);
@@ -126,7 +139,7 @@ int main(int argc, char **argv)
         double cpuTime = 0.001 * sdkGetTimerValue(&hTimer) / (double)iterations;
         printf("convolutionSeparableCPU, Throughput = %.4f MPixels/sec, Time = %.9f s, Size = %u Pixels\n\n",
                (1.0e-6 * (double)(imageW * imageH)/ cpuTime), cpuTime, (imageW * imageH));
-    }
+    }*/
 
     // Separable Convolution on GPU
     {
@@ -180,6 +193,7 @@ int main(int argc, char **argv)
         checkCudaErrors(cudaFree(d_Input));
     }
 
+	/*
     // Simple Kernel straigtforward full approach
     {
         checkCudaErrors(cudaMalloc((void **)&d_Input,   imageW * imageH * sizeof(float)));
@@ -202,7 +216,15 @@ int main(int argc, char **argv)
                 sdkStartTimer(&hTimer);
             }
 
-            convolutionFullNaive(gridSize, blockSize, d_Input, d_Output, d_Kernel, imageW, imageH, KERNEL_RADIUS);
+            convolutionFullNaive(
+				gridSize, 
+				blockSize, 
+				d_Input, 
+				d_Output, 
+				d_Kernel, 
+				imageW, 
+				imageH, 
+				KERNEL_RADIUS);
         }
 
         checkCudaErrors(cudaDeviceSynchronize());
@@ -216,7 +238,7 @@ int main(int argc, char **argv)
         checkCudaErrors(cudaFree(d_Kernel));
         checkCudaErrors(cudaFree(d_Output));
         checkCudaErrors(cudaFree(d_Input));
-    }
+    }*/
 
     // Simple Kernel straigtforward separable approach
     {
@@ -303,6 +325,18 @@ int main(int argc, char **argv)
         checkCudaErrors(cudaFree(d_Input));
     }
 
+	seperableSharedKernelUnroll(
+		d_Input,
+		d_Output,
+		d_Buffer,
+		d_Kernel,
+		h_Kernel,
+		h_Input,
+		h_OutputGPUSepShared,
+		imageW,
+		imageH,
+		iterations,
+		hTimer);
 
 
  /*   for (int i = 0; i < imageW; ++i)
@@ -339,6 +373,61 @@ int main(int argc, char **argv)
     sdkDeleteTimer(&hTimer);
 
     exit(EXIT_SUCCESS);
+}
+
+void seperableSharedKernelUnroll(
+	float *d_Input,
+	float *d_Output,
+	float *d_Buffer,
+	float *d_Kernel,
+	float *h_Kernel,
+	float *h_Input,
+	float *h_OutputGPUSepShared,
+	const int imageW,
+	const int imageH,
+	const int iterations,
+	StopWatchInterface *hTimer)
+{
+		checkCudaErrors(cudaMalloc((void **)&d_Input, imageW * imageH * sizeof(float)));
+		checkCudaErrors(cudaMalloc((void **)&d_Output, imageW * imageH * sizeof(float)));
+		checkCudaErrors(cudaMalloc((void **)&d_Kernel, KERNEL_LENGTH * sizeof(float)));
+		checkCudaErrors(cudaMemcpy(d_Input, h_Input, imageW * imageH * sizeof(float), cudaMemcpyHostToDevice));
+		checkCudaErrors(cudaMemcpy(d_Kernel, h_Kernel, KERNEL_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
+
+		checkCudaErrors(cudaMalloc((void **)&d_Buffer, imageW * imageH * sizeof(float)));
+
+
+		dim3 gridSize(isDividedUp(imageH, 16), isDividedUp(imageW, 16));
+		dim3 blockSize(16, 16);
+
+		printf("Running GPU naive separable convolution (%u identical iterations).\n", iterations);
+		for (int i = -1; i < iterations; i++)
+		{
+			//i == -1 -- warmup iteration
+			if (i == 0)
+			{
+				checkCudaErrors(cudaDeviceSynchronize());
+				sdkResetTimer(&hTimer);
+				sdkStartTimer(&hTimer);
+			}
+
+			convolutionSeparableRowSharedUnroll(gridSize, blockSize, d_Input, d_Buffer, d_Kernel, imageW, imageH);
+			convolutionSeparableColumnSharedUnroll(gridSize, blockSize, d_Buffer, d_Output, d_Kernel, imageW, imageH);
+			//convolutionSeparableRowShared(gridSize, blockSize, d_Input, d_Buffer, d_Kernel, imageW, imageH, KERNEL_RADIUS);
+			//convolutionSeparableColumnShared(gridSize, blockSize, d_Buffer, d_Output, d_Kernel, imageW, imageH, KERNEL_RADIUS);
+		}
+
+		checkCudaErrors(cudaDeviceSynchronize());
+		sdkStopTimer(&hTimer);
+		double cpuTime = 0.001 * sdkGetTimerValue(&hTimer) / (double)iterations;
+		printf("convolutionSeparableSharedUnrollGPU, Throughput = %.4f MPixels/sec, Time = %.9f s, Size = %u Pixels\n\n",
+			(1.0e-6 * (double)(imageW * imageH) / cpuTime), cpuTime, (imageW * imageH));
+
+		checkCudaErrors(cudaMemcpy(h_OutputGPUSepShared, d_Output, imageW * imageH * sizeof(float), cudaMemcpyDeviceToHost));
+
+		checkCudaErrors(cudaFree(d_Kernel));
+		checkCudaErrors(cudaFree(d_Output));
+		checkCudaErrors(cudaFree(d_Input));
 }
 
 bool imgL2error(float* img1, float* img2, const int & imageW, const int & imageH)
